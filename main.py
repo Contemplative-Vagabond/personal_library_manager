2b3edcdce918eee4d871ec639424afce3dd282be
Contemplative-Vagabond <dg.contemplativevagabond@gmail.com>
2025-03-11 17:57:17 +0100

    started trying to add functonality and added 'privat' directory for library to .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+
+#class for library
+class Library:
+    def __init__(self, path_to_file):
+        self.inventory = ""
+        with open(path_to_file) as f:
+            text = f.read
+            index = 0
+            for line in text.split():
+                self.inventory[index] = line
+                index += 1
+
+
+class Shelf(Library):
+    def __init__(self):
+        super().__init__()
+
+#class for book
+class Book:
+    def __init__(self):
+        pass
+
+    #function for adding book to library
+    def add_book():
+        pass 
+
+path = "library/inventory.csv"
+lib = Library(path)
+print(lib.inventory)
+
 print("your books here...")
